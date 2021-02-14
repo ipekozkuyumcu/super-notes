b165f96b3a8810eddcaa4d6b4f45352fc8bc7178
Ipek Ozkuyumcu <ipekozkuyumcu@hotmail.com>
2021-02-14 17:37:35 +0300

    feat: icons

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 export const environment = {
     production: true,
     protocol: 'https://',
-    host: 'ipekozkuyumcu.com',
+    host: '----.com',
     defaultData: {
         defaultTitle: '',
         defaultDescription: '',
@@ -12,14 +12,5 @@ export const environment = {
         'fb:admins': undefined
     },
     Angulartics2: {},
-    firebase: {
-        apiKey: 'AIzaSyCMhFZBQm0sDSs-duP9zoZPJmv6cN-3wbg',
-        authDomain: 'super-notes.firebaseapp.com',
-        databaseURL: 'https://super-notes.firebaseio.com',
-        projectId: 'super-notes',
-        storageBucket: 'super-notes.appspot.com',
-        messagingSenderId: '940887124494',
-        appId: '1:940887124494:web:e6bd307affecb899a8ea5f',
-        measurementId: 'G-7E5DNGGHWL'
-    }
+    firebase: undefined
 };
